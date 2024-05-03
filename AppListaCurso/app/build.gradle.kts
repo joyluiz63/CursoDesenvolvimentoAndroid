84d93e903c17770e86bc8eb7a3d303bfaee00950
Joy Silva <joyluiz63@gmail.com>
2024-05-03 19:16:23 -0300

    Estrutura do projeto. Pastas App e Gradle

--- AppListaCurso/app/build.gradle.kts
+++ AppListaCurso/app/build.gradle.kts
@@ -12,8 +12,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -35,9 +33,6 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
-    implementation(libs.activity)
     implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+
 }
